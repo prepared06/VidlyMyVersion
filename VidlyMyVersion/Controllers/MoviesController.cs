dfe7cbe61784a1a9116fde636d341b1634d7d1ad
Konstantin Didenko <ironbullyk@gmail.com>
2021-11-02 20:30:18 +0200

    fist and second lessons b4 exercise

---
+++ VidlyMyVersion/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyMyVersion.Models;
+using VidlyMyVersion.ViewModels;
+
+namespace VidlyMyVersion.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek!"
+            };
+
+
+            var customers = new List<Customer>
+            {
+                new Customer{ Name="Customer 1" },
+                new Customer{ Name="Customer 1"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
