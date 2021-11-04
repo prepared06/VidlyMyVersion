bad248aa8e25a52a891b62b6806837271e537b63
Konstantin Didenko <ironbullyk@gmail.com>
2021-11-04 15:53:48 +0200

    First exercise 'Display membership type' from section 3

---
+++ VidlyMyVersion/Migrations/202111041334235_PopulateNameToMembershipType.cs
@@ -0,0 +1,29 @@
+namespace VidlyMyVersion.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateNameToMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes " +
+                "SET Name='Pay as You Go' " +
+                "WHERE Id='1'");
+            Sql("UPDATE MembershipTypes " +
+                "SET Name='Monthly' " +
+                "WHERE Id='2'");
+            Sql("UPDATE MembershipTypes " +
+                "SET Name='Quarterly' " +
+                "WHERE Id='3'");
+            Sql("UPDATE MembershipTypes " +
+                "SET Name='Annual', DiscountRate='20' " +
+                "WHERE Id='4'");
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
