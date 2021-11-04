bc4a6059653910d4ccc85c8f40859b97bdc1c75f
Konstantin Didenko <ironbullyk@gmail.com>
2021-11-05 01:49:59 +0200

    Second exercise from section 3

--- VidlyMyVersion/Models/Customer.cs
+++ VidlyMyVersion/Models/Customer.cs
@@ -12,6 +12,7 @@ namespace VidlyMyVersion.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        public DateTime? Birthday { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
